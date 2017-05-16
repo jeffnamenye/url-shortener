c2c2b4f85fc8efe873750e2594d61015db55178e
Jeff Namenye <jeffnamenye@hotmail.com>
2017-05-16 19:17:01 -0400

    added to test files

---
+++ test/__models_data_urlShort.js
@@ -0,0 +1,11 @@
+onst expect = require('chai').expect;
+const Url = require('../models/data/urlShort');
+
+describe('url model', () => {
+  it('Create url', () => {});
+  it('Read all urls', () => {});
+  it('Read url by id', () => {});
+  it('Redirect', () => {});
+  it('Update url', () => {});
+  it('Delete url', () => {});
+});
---
+++ test/__models_data_user.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const User = require('../models/data/user');
+
+describe('user model', () => {
+  it('Create user', () => {});
+  it('Read all users', () => {});
+  it('Read user by id', () => {});
+  it('Update user', () => {});
+  it('Delete user', () => {});
+});
